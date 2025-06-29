97c1d1756c1ae4211a9570e8c286de3f23cae9da
Sofija <122399651+sofijazolotarev@users.noreply.github.com>
2025-06-30 00:07:17 +0200

    Add files via upload

---
+++ usersAppTests/usersAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  usersAppTests.swift
+//  usersAppTests
+//
+//  Created by Sofija Zolotarev on 30.4.25..
+//
+
+import Testing
+@testable import usersApp
+
+struct usersAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
